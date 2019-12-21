f352b8466566921099c9492c161d3ed9fef6b5f9
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-12-21 15:14:17 +0900

    #1.2 Serializers

---
+++ rooms/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+class RoomSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=140)
+    price = serializers.IntegerField()
+    bedrooms = serializers.IntegerField()
+    instant_book = serializers.BooleanField()
--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Room
+from .serializers import RoomSerializer
 
 
 @api_view(["GET"])
 def list_rooms(request):
-    return Response()
+    rooms = Room.objects.all()
+    serialized_rooms = RoomSerializer(rooms, many=True)
+    return Response(data=serialized_rooms.data)
