b0e32969e0d25622f934ce3e5dd405f7f7afdfff
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-08 19:04:40 +0900

    #2.15 Searching Part One

--- rooms/views.py
+++ rooms/views.py
@@ -75,8 +75,27 @@ class RoomView(APIView):
 
 @api_view(["GET"])
 def room_search(request):
+    max_price = request.GET.get("max_price", None)
+    min_price = request.GET.get("min_price", None)
+    beds = request.GET.get("beds", None)
+    bedrooms = request.GET.get("bedrooms", None)
+    bathrooms = request.GET.get("bathrooms", None)
+    filter_kwargs = {}
+    if max_price is not None:
+        filter_kwargs["price__lte"] = max_price
+    if min_price is not None:
+        filter_kwargs["price__gte"] = min_price
+    if beds is not None:
+        filter_kwargs["beds__gte"] = beds
+    if bedrooms is not None:
+        filter_kwargs["bedrooms__gte"] = bedrooms
+    if bathrooms is not None:
+        filter_kwargs["bathrooms__gte"] = bathrooms
     paginator = OwnPagination()
-    rooms = Room.objects.filter()
+    try:
+        rooms = Room.objects.filter(**filter_kwargs)
+    except ValueError:
+        rooms = Room.objects.all()
     results = paginator.paginate_queryset(rooms, request)
     serializer = RoomSerializer(results, many=True)
     return paginator.get_paginated_response(serializer.data)
