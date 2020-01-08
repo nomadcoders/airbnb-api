d459f76600a9f6bb03a7a8d8781ce116c9d49851
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-08 19:15:24 +0900

    #2.16 Searching Part Two

--- rooms/views.py
+++ rooms/views.py
@@ -80,6 +80,8 @@ def room_search(request):
     beds = request.GET.get("beds", None)
     bedrooms = request.GET.get("bedrooms", None)
     bathrooms = request.GET.get("bathrooms", None)
+    lat = request.GET.get("lat", None)
+    lng = request.GET.get("lng", None)
     filter_kwargs = {}
     if max_price is not None:
         filter_kwargs["price__lte"] = max_price
@@ -92,6 +94,11 @@ def room_search(request):
     if bathrooms is not None:
         filter_kwargs["bathrooms__gte"] = bathrooms
     paginator = OwnPagination()
+    if lat is not None and lng is not None:
+        filter_kwargs["lat__gte"] = float(lat) - 0.005
+        filter_kwargs["lat__lte"] = float(lat) + 0.005
+        filter_kwargs["lng__gte"] = float(lng) - 0.005
+        filter_kwargs["lng__lte"] = float(lng) + 0.005
     try:
         rooms = Room.objects.filter(**filter_kwargs)
     except ValueError:
