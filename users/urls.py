5eb108e1990f7217f2d8275624c0d414f9a34b02
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-09 18:23:35 +0900

    #3.5 Users Viewset

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,8 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
 app_name = "users"
 
-urlpatterns = [
-    path("", views.UsersView.as_view()),
-    path("token/", views.login),
-    path("me/", views.MeView.as_view()),
-    path("me/favs/", views.FavsView.as_view()),
-    path("<int:pk>/", views.user_detail),
-]
+router = DefaultRouter()
+router.register("", views.UsersViewSet)
+urlpatterns = router.urls
--- users/views.py
+++ users/views.py
@@ -6,20 +6,27 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser, AllowAny
 from rooms.serializers import RoomSerializer
 from rooms.models import Room
 from .models import User
 from .serializers import UserSerializer
 
 
-class UsersView(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            new_user = serializer.save()
-            return Response(UserSerializer(new_user).data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UsersViewSet(ModelViewSet):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == "list":
+            permission_classes = [IsAdminUser]
+        elif self.action == "create" or self.action == "retrieve":
+            permission_classes = [AllowAny]
+
+        return [permission() for permission in permission_classes]
 
 
 class MeView(APIView):
